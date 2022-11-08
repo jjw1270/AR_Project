b57113282878ec093551b07992768571aa6bad1f
Yunjae Jang <jjw1270@naver.com>
2022-11-08 19:35:17 +0900

    STT 기능 삭제
    vr 포기로 stt의 필요가 없어짐으로 제거.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-#if PLATFORM_ANDROID
-using UnityEngine.Android;
-#endif
-
 
 public class GameManager : Singleton<GameManager>
 {
-    [SerializeField]SpeechToText speechToText;
     [SerializeField]TextToSpeech textToSpeech;
     void Start()
     {
@@ -17,11 +12,7 @@ public class GameManager : Singleton<GameManager>
 
     void Update()
     {
-        if(Input.GetMouseButtonDown(0)){
-            //화면 터치 시 음성 녹음 시작 -> 자동으로 음성 녹음 종료, 해당하는 명령 실행
-            //이미 녹음 중인데 또 터치하면 동작 x
-            speechToText.StartRecording();
-        }
+        
     }
 
     public void CommandExecution(string result){
