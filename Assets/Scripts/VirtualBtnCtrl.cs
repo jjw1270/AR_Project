c23a9e32e5689a414c319898b9591189af2fccb5
jjw1270 <jjw1270@naver.com>
2022-11-08 12:19:59 +0900

    .

---
+++ Assets/Scripts/VirtualBtnCtrl.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualBtnCtrl : MonoBehaviour
+{
+    private VirtualButtonBehaviour vb;
+    private void Start() {
+        vb = this.GetComponent<VirtualButtonBehaviour>();
+        vb.RegisterOnButtonPressed(OnButtonPressed);
+        vb.RegisterOnButtonReleased(OnButtonReleased);
+        Debug.Log("asd");
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb){
+        Debug.Log(this.gameObject.name + " 버튼 눌림");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb){
+        Debug.Log(this.gameObject.name + " 버튼 떼짐");
+    }
+}
